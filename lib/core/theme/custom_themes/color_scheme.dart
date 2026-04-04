a6bd0b2a0d093924cd2909604103d3287a5ff769
Thean Yee Sin <yeesinthean@outlook.com>
2026-04-04 15:15:24 +0800

    chore: added more theme related

---
+++ lib/core/theme/custom_themes/color_scheme.dart
@@ -0,0 +1,9 @@
+import "package:flutter/material.dart";
+
+class TColorScheme {
+  static ColorScheme lightColorScheme() =>
+      ColorScheme.fromSeed(seedColor: const Color(0xFF6750A4));
+
+  static ColorScheme darkColorScheme() =>
+      ColorScheme.fromSeed(seedColor: const Color(0xFF6750A4));
+}
--- lib/core/theme/theme.dart
+++ lib/core/theme/theme.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 import "package:tick_tock/core/theme/custom_themes/app_bar_theme.dart";
+import "package:tick_tock/core/theme/custom_themes/color_scheme.dart";
 
 class TAppTheme {
   TAppTheme._();
@@ -7,10 +8,12 @@ class TAppTheme {
   static ThemeData lightTheme() => ThemeData(
         useMaterial3: true,
         appBarTheme: TAppBarTheme.lightAppBarTheme(),
+        colorScheme: TColorScheme.lightColorScheme(),
       );
 
   static ThemeData darkTheme() => ThemeData(
         useMaterial3: true,
         appBarTheme: TAppBarTheme.darkAppBarTheme(),
+        colorScheme: TColorScheme.darkColorScheme(),
       );
 }
